e6e43fcc08be4aceed1204c334ec375ee06b8efd
Kip Cole <kip@Kip-macbook.local>
2010-10-17 02:51:58 +0800

    Woops, missed a rename

--- lib/ar_to_html_table.rb
+++ lib/ar_to_html_table.rb
@@ -3,8 +3,8 @@ require File.dirname(__FILE__) + '/ar_to_html_table/table_formatter.rb'
 require File.dirname(__FILE__) + '/ar_to_html_table/html_table.rb'
 require File.dirname(__FILE__) + '/ar_to_html_table/column_formats.rb'
 
-Array.send :include, HtmlTable::Model
-ActiveRecord::Base.send :include, HtmlTable::ColumnFormats
+Array.send :include, ArHtmlTable::Model
+ActiveRecord::Base.send :include, ArHtmlTable::ColumnFormats
 I18n.load_path += Dir[ File.join(File.dirname(__FILE__), 'locale', '*.{rb,yml}') ]
 
 module ArToHtmlTable
