84684b8d704486279ce601e004bb935ecce9f180
Kip Cole <kip@Kip-macbook.local>
2010-10-17 02:55:55 +0800

    Shouldn't refactor at 3am

--- lib/ar_to_html_table.rb
+++ lib/ar_to_html_table.rb
@@ -1,10 +1,10 @@
 require File.dirname(__FILE__) + '/ar_to_html_table/column_formatter.rb'
 require File.dirname(__FILE__) + '/ar_to_html_table/table_formatter.rb'
-require File.dirname(__FILE__) + '/ar_to_html_table/html_table.rb'
 require File.dirname(__FILE__) + '/ar_to_html_table/column_formats.rb'
+require File.dirname(__FILE__) + '/ar_to_html_table/model.rb'
 
-Array.send :include, ArHtmlTable::Model
-ActiveRecord::Base.send :include, ArHtmlTable::ColumnFormats
+Array.send :include, ArToHtmlTable::Model
+ActiveRecord::Base.send :include, ArToHtmlTable::ColumnFormats
 I18n.load_path += Dir[ File.join(File.dirname(__FILE__), 'locale', '*.{rb,yml}') ]
 
 module ArToHtmlTable
