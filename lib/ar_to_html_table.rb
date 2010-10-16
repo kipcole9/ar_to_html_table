c73de182b0abe72a3f469565f1302688736accd6
Kip Cole <kip@Kip-macbook.local>
2010-10-17 02:37:57 +0800

    Refactor ColumnFormat module and directories

--- lib/ar_to_html_table.rb
+++ lib/ar_to_html_table.rb
@@ -1,8 +1,8 @@
-require File.dirname(__FILE__) + '/html_table/column_formatter.rb'
-require File.dirname(__FILE__) + '/html_table/table_formatter.rb'
-require File.dirname(__FILE__) + '/html_table/html_table.rb'
-require File.dirname(__FILE__) + '/column_formats.rb'
+require File.dirname(__FILE__) + '/ar_to_html_table/column_formatter.rb'
+require File.dirname(__FILE__) + '/ar_to_html_table/table_formatter.rb'
+require File.dirname(__FILE__) + '/ar_to_html_table/html_table.rb'
+require File.dirname(__FILE__) + '/ar_to_html_table/column_formats.rb'
 
-Array.send :include, HtmlTable
-ActiveRecord::Base.send :include, ColumnFormats
+Array.send :include, HtmlTable::Model
+ActiveRecord::Base.send :include, HtmlTable::ColumnFormats
 I18n.load_path += Dir[ File.join(File.dirname(__FILE__), 'locale', '*.{rb,yml}') ]
